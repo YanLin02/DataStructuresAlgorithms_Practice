63fdfa5a473befd7da6826858ef8097d58de0569
Sun Yanlin <1310400098@qq.com>
2023-09-25 16:04:53 +0800

    Buffer init

--- MatrixMultiplication/Buffer.h
+++ MatrixMultiplication/Buffer.h
@@ -1,13 +1,29 @@
+
 #pragma once
 #include "Definition.h"
 
 #include <unordered_map>
 
+using namespace std;
 
+typedef pair<unsigned int, unsigned int> Pa2;
+
+/// @brief Pa2's hash, used for hashmap
+struct Pa2_hash
+{
+	template <class T, class U>
+	int operator()(const pair<T, U>& a) const
+	{
+		return hash<int>()(a.first) * 17 ^ hash<int>()(a.second) * 71;
+	}
+};
+
+/// @brief Êý×é»º´æ
 class Buffer
 {
 public:
 
-	Buffer() = default;
+	unordered_map<Pa2, double, Pa2_hash> Buf;
+	
 };
 
