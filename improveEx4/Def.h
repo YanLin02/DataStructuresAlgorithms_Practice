00e0def008777b5d2da2ccd7b493a7e6634d05ef
Sun Yanlin <1310400098@qq.com>
2023-10-29 22:44:45 +0800

    建立项目

---
+++ improveEx4/Def.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#define BUFFER_SIZE 7
+#define TREE_HEIGHT 5
+
+#define TEST_SIZE 100
+
+#define TEMP_FILE_NAME "temp.bin"
---
+++ improveEx4/main.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+#include "MergeSort.h"
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	//srand(202310353);
+
+	int* arr = new int[TEST_SIZE];
+	for (int i = 0; i < TEST_SIZE; i++)
+	{
+		arr[i] = rand() % 100;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	ofstream t("test.bin", ios::binary);
+	t.write((char*)arr, sizeof(int) * TEST_SIZE);
+	t.close();
+
+
+	sort(arr, arr + TEST_SIZE);
+	for (int i = 0; i < TEST_SIZE; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+
+	MergeSort test("test.bin");
+	test.Sort();
+
+	int temp;
+	ifstream f("test.bin", ios::binary);
+	while (f.read((char*)&temp, sizeof(int)))
+		cout << temp << " ";
+
+
+	//system("pause");
+	return 0;
+}
