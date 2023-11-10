598e9f84bb73bca899063be76289ba7519db5146
Sun Yanlin <1310400098@qq.com>
2023-11-11 01:59:26 +0800

    文件整理

--- ExternalSort_MergeSort/报告包/Defination.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-#define INPUT_BUFFER_SIZE 10 ///<输入缓存大小
-//#define OUTPUT_BUFFER_SIZE (2 * INPUT_BUFFER_SIZE); ///<输出缓存大小
-
-#define TEST_FILE_SIZE 10 ///<测试文件大小
-#define TEST_FILE_NAME "test.bin" ///<测试文件名
-#define TEST_NUM_MAX 100 ///<测试数最大值
--- MatrixMultiplication/报告包/Definition.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define BUFFER_SIZE 10 //缓存大小
-#define MATRIX_SIZE 10 //矩阵大小
-
